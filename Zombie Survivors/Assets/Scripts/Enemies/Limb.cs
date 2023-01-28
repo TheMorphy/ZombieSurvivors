479cbb537e22a317156ab3b231e81c5b08db7c57
Arnas T <49756303+AarnasS75@users.noreply.github.com>
2023-01-28 23:36:21 +0200

    Limbs models adjustments

--- Zombie Survivors/Assets/Scripts/Enemies/Limb.cs
+++ Zombie Survivors/Assets/Scripts/Enemies/Limb.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 public class Limb : MonoBehaviour
 {
 	[SerializeField] private GameObject limbPrefab;
-	private Limb[] childLimbs;
-
 	private Enemy enemy;
-	CharacterJoint joint;
-	Rigidbody rb;
 
 	private void Awake()
 	{
-		//childLimbs = GetComponentsInChildren<Limb>();
 		enemy = transform.root.GetComponentInParent<Enemy>();
-		joint = GetComponent<CharacterJoint>();
-		rb = GetComponent<Rigidbody>();
 	}
 
 	public void GetHit(int damageAmmount)
@@ -25,18 +18,9 @@ public class Limb : MonoBehaviour
 
 	public void RemoveLimb()
 	{
-		//for (int i = 0; i < childLimbs.Length; i++)
-		//{
-		//	if (childLimbs[i] != null)
-		//	{
-		//		childLimbs[i].transform.localScale = Vector3.zero;
-		//		Destroy(childLimbs[i]);
-		//	}
-		//}
+		GameObject limb = Instantiate(limbPrefab, transform.position, transform.rotation);
 
-		Instantiate(limbPrefab, transform.position, transform.rotation);
-
-		//limb.GetComponent<Rigidbody>().AddForce(transform.forward * 10f);
+		limb.GetComponent<Rigidbody>().AddForce(transform.forward * 4f);
 
 		transform.localScale = Vector3.zero;
 
